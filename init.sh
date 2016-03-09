df7b27d47712e4260d13aee6d706879583737c00
sda-stepic <_sda@tut.by>
2016-03-10 01:52:26 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,7 +1,7 @@
 sudo rm /etc/nginx/sites-enabled/default
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
-#sudo ln -sf /home/box/web/etc/gunicorn.conf
-sudo ln -sf /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
+#sudo ln -sf /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
+sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/test
 sudo /etc/init.d/gunicorn restart
 
